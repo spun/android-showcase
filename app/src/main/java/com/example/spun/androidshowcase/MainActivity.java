474330a68ce60cd3f2ea2ab4d89eaaa83139d896
spun <neoappengine@gmail.com>
2015-09-02 20:02:19 +0200

    Añade una lista para las actividades elegibles

--- app/src/main/java/com/example/spun/androidshowcase/MainActivity.java
+++ app/src/main/java/com/example/spun/androidshowcase/MainActivity.java
@@ -4,13 +4,48 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> mActivitiesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Lista de actividades
+        String[] data = {
+                "Activity 1",
+                "Activity 2"
+        };
+        List<String> activitiesList = new ArrayList<>(Arrays.asList(data));
+
+        mActivitiesAdapter =
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        activitiesList);
+
+        ListView listView = (ListView) findViewById(R.id.listview_activities);
+        listView.setAdapter(mActivitiesAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String activity = mActivitiesAdapter.getItem(position);
+                Toast.makeText(getApplicationContext(), activity, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
