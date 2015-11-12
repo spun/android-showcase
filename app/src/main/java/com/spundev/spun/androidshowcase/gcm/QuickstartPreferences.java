7faab96f3f1cb3e9ecc8da33e7527300c098272d
spun <neoappengine@gmail.com>
2015-11-12 20:44:36 +0100

    Simplifica el nombre del paquete asignado

--- app/src/main/java/com/spundev/spun/androidshowcase/gcm/QuickstartPreferences.java
+++
@@ -1,8 +0,0 @@
-package com.spundev.spun.androidshowcase.gcm;
-
-public class QuickstartPreferences {
-
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
-    public static final String REGISTRATION_COMPLETE = "registrationComplete";
-
-}
