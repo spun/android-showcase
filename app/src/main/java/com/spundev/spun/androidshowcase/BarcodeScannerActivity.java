c5b380686a3a120b2a7cd072ea710e6510a39068
spun <neoappengine@gmail.com>
2015-11-12 16:18:35 +0100

    Añade a la actividad la petición de permisos en tiempo de ejecución de android 6

--- app/src/main/java/com/spundev/spun/androidshowcase/BarcodeScannerActivity.java
+++ app/src/main/java/com/spundev/spun/androidshowcase/BarcodeScannerActivity.java
@@ -1,6 +1,10 @@
 package com.spundev.spun.androidshowcase;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
@@ -9,6 +13,7 @@ import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
@@ -23,12 +28,59 @@ public class BarcodeScannerActivity extends AppCompatActivity {
     private CameraSource mCameraSource;
     private SurfaceView mCameraView;
     private TextView mBarcodeInfo;
+    private static final int REQUEST_CAMERA = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barcode_scanner);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check if the Camera permission is already avaible.
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission is already avaible, show the camera preview.
+                showCameraPreview();
+            } else {
+                // Camera permission has not been granted.
+
+                // Provide an additional rationale to the user if the permission was not granted
+                // and the user would benefit from additional context for the use of the permission.
+                if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                    Toast.makeText(this, "Camera permission is needed to show the camera preview", Toast.LENGTH_LONG).show();
+                }
+
+                // Request Camera permission
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+            }
+        } else {
+            showCameraPreview();
+        }
+
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CAMERA) {
+            // Received permission result for camera permission.
+
+            // Check if the only required permission has been granted
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission has been granted, preview can be displayed
+                showCameraPreview();
+            } else {
+                // Camera permission was denied, so we cannot use this feature
+                Toast.makeText(this, "Permission was not granted", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    void showCameraPreview() {
         mCameraView = (SurfaceView) findViewById(R.id.camera_view);
         mBarcodeInfo = (TextView) findViewById(R.id.code_info);
 
